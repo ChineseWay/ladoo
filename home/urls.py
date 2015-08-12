64e44f6a35f7ad2403e643b4ddc0571dd990b6e3
zhuangzebo <zebozhuang@163.com>
2015-08-13 01:25:57 +0800

    add page

--- home/urls.py
+++ home/urls.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from home.views import HomeHandler
+from home.views import EnglishProfileHandler
+from home.views import ChineseProfileHandler
 
 
 urls = [
     (r"/", HomeHandler),       
+    (r"/profile/en/?", EnglishProfileHandler),
+    (r"/profile/ch/?", ChineseProfileHandler),
 ]
--- home/views.py
+++ home/views.py
@@ -8,3 +8,13 @@ from utils.utils import BasicHandler
 class HomeHandler(BasicHandler):
     def get(self):
         self.render("index.html")
+
+
+class EnglishProfileHandler(BasicHandler):
+    def get(self):
+        self.render("profile_en.html")
+
+
+class ChineseProfileHandler(BasicHandler):
+    def get(self):
+        self.render("profile_ch.html")
